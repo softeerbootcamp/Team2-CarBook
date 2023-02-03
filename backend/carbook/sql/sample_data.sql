0619a6040aa6f743f3f3ee3b4a2c81801d7ea8f8
Soomin-Lim <augustth511@gmail.com>
2023-02-03 18:20:36 +0900

    docs: 사용자와 게시물 샘플 데이터를 삽입하고 조회하는 sql문 추가

---
+++ backend/carbook/sql/sample_data.sql
@@ -0,0 +1,6 @@
+insert into user(email, password, nickname) values("abc@gamil.com", "password", "hello");
+
+insert into post(user_id, create_date, update_date, content) values(1, '2023-01-11', '2023-01-11', "hi");
+
+select * from user;
+select * from post;
