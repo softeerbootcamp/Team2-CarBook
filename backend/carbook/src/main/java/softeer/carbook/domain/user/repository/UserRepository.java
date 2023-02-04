aded0e7f6ecb879c05f48c63383a59b6185f16f2
jhchoi57 <enough1125@gmail.com>
2023-02-04 23:29:24 +0900

    feat: UserRepository addUser 기능 구현

--- backend/carbook/src/main/java/softeer/carbook/domain/user/repository/UserRepository.java
+++ backend/carbook/src/main/java/softeer/carbook/domain/user/repository/UserRepository.java
@@ -1,16 +1,42 @@
 package softeer.carbook.domain.user.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+import softeer.carbook.domain.user.dto.SignupForm;
+import softeer.carbook.domain.user.model.User;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Repository
 public class UserRepository {
-    private final DataSource dataSource;
+    private final JdbcTemplate jdbcTemplate;
 
+    @Autowired
     public UserRepository(DataSource dataSource){
-        this.dataSource = dataSource;
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
+
+    public void addUser(SignupForm signupForm) {
+        jdbcTemplate.query("insert into user values(?, ?, ?)", userRowMapper());
+    }
+
+    private RowMapper<User> userRowMapper(){
+        return new RowMapper<User>() {
+            @Override
+            public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                User user = new User(
+                        rs.getString("email");
+                        rs.getString("nickname");
+                        rs.getString("password");
+                );
+                return user;
+            }
+        };
+    }
+
 
 }
