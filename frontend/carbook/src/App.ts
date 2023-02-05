15f03d1282697d7ce4a4330417187523c25473a6
Dongja <jdh981118@naver.com>
2023-02-05 23:58:24 +0900

    💄design: 추가 로그인 페이지 초기 마크업

--- frontend/carbook/src/App.ts
+++ frontend/carbook/src/App.ts
@@ -1,8 +1,13 @@
 import { Component } from '@/core';
 import Router from '@/utils/router';
+import { LoginPage } from './pages';
 
 export default class App extends Component {
   setup(): void {
     new Router(this.$target);
+  }
+  template(): string {
+    const loginpage = new LoginPage(document.body);
+    return loginpage.template();
   }
 }
