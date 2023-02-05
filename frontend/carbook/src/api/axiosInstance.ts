26d7e5bb06ccf38192e55e6ea5315db9c2db19e9
Dongja <jdh981118@naver.com>
2023-02-06 00:28:16 +0900

    ⚒refactor: set Timedut and divide Content

--- frontend/carbook/src/api/axiosInstance.ts
+++ frontend/carbook/src/api/axiosInstance.ts
@@ -2,7 +2,22 @@ import axios from 'axios';
 
 const BASEURL = '';
 
-export const axiosInstance = axios.create({
+export const axiosAppFormInstance = axios.create({
   withCredentials: true,
   baseURL: BASEURL,
+  timeout: 3000,
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded;',
+    Accept: '*/*',
+  },
+});
+
+export const axiosMultiFormInstance = axios.create({
+  withCredentials: true,
+  baseURL: BASEURL,
+  timeout: 3000,
+  headers: {
+    'Content-Type': 'multipart/form-data;',
+    Accept: '*/*',
+  },
 });
