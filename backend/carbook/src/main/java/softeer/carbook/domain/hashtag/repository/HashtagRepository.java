a7e6314ba6e3b00e92541c17c4cff320489de5e8
Soomin-Lim <augustth511@gmail.com>
2023-02-06 15:12:27 +0900

    feat: HashtagRepository에서 태그 이름으로 Hashtag를 조회하는 메서드 추가
    - findHashtagByName(): 태그 이름으로 Hashtag를 조회

--- backend/carbook/src/main/java/softeer/carbook/domain/hashtag/repository/HashtagRepository.java
+++ backend/carbook/src/main/java/softeer/carbook/domain/hashtag/repository/HashtagRepository.java
@@ -1,4 +1,33 @@
 package softeer.carbook.domain.hashtag.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import softeer.carbook.domain.hashtag.model.Hashtag;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Optional;
+
 public class HashtagRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public HashtagRepository(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public Optional<Hashtag> findHashtagByName(String tag) {
+        List<Hashtag> hashtags = jdbcTemplate.query("select * from HASHTAG where tag = ?", tagRowMapper(), tag);
+        return hashtags.stream()
+                .findAny();
+    }
+
+    private RowMapper<Hashtag> tagRowMapper() {
+        return (rs, rowNum) -> new Hashtag(
+                rs.getInt("id"),
+                rs.getString("tag")
+        );
+    }
 }
