49dbb5c0e4c595b84d94624446f25b56ae083a0f
EUNA <67576476+beni1026@users.noreply.github.com>
2023-02-06 15:36:39 +0900

    fix: 이미지 경로 수정

--- frontend/carbook/src/pages/Login/Login.ts
+++ frontend/carbook/src/pages/Login/Login.ts
@@ -1,28 +1,30 @@
 import { Component } from '@/core';
 import './Login.scss';
-import car from '@/assets/car.svg';
+import car from '@/assets/images/car.svg';
 
 export default class LoginPage extends Component {
   template(): string {
     return `
-    <header>
-      <img src = ${car} class ='header-image'>
-    <h1 class = 'header-logo'>CarBook</h1>
-    <h3 class = 'header-message'>카북에 오신걸 환영합니다</h3>
-    </header>
-    <div class='login-contents'>
-      <form class ='input-form'>
-        <div class = 'login-id'> ID</div>
-        <input type = 'text' placeholder='id를 입력해주세요' class ='input-box' name ='login-id'/>
-        <div class ='login-password'> Password</div>
-        <input type = 'password' class ='input-box' name = 'login-password' placeholder='비밀번호를 입력해주세요'/>
-        <button type = 'submit' class ='input-form-button'>로그인</button>
-      </form>
+    <div class="login__container">
+      <header>
+        <img src = ${car} class ='header-image'>
+      <h1 class = 'header-logo'>CarBook</h1>
+      <h3 class = 'header-message'>카북에 오신걸 환영합니다</h3>
+      </header>
+      <div class='login-contents'>
+        <form class ='input-form'>
+          <div class = 'login-id'> ID</div>
+          <input type = 'text' placeholder='id를 입력해주세요' class ='input-box' name ='login-id'/>
+          <div class ='login-password'> Password</div>
+          <input type = 'password' class ='input-box' name = 'login-password' placeholder='비밀번호를 입력해주세요'/>
+          <button type = 'submit' class ='input-form-button'>로그인</button>
+        </form>
+      </div>
+      <footer>
+        <h3 class ='footer-message'>계정이 없으신가요?</h3>
+        <h3 class ='register'>회원 가입</h3>
+      </footer>
     </div>
-    <footer>
-      <h3 class ='footer-message'>계정이 없으신가요?</h3>
-      <h3 class ='register'>회원 가입</h3>
-    </footer>
     `;
   }
 }
