0e48cc4a22e17a0660b328c2d4ca2e3808d43469
EUNA <67576476+beni1026@users.noreply.github.com>
2023-02-06 15:06:10 +0900

    feat: 메인페이지 마크업

--- frontend/carbook/src/pages/Home/Home.ts
+++ frontend/carbook/src/pages/Home/Home.ts
@@ -1,6 +1,52 @@
 import { Component } from '@/core';
+import './Home.scss';
+
 export default class HomePage extends Component {
   template(): string {
-    return `<div>홈페이지 입니다</div>`;
+    return `
+    <div class="home-container">
+      <header class="header">
+        <div class="header__left-box">
+          <div class="header__logo">CarBook</div>
+          <div class="header__sub">차 사진의 모든 것</div>
+        </div>
+        <div class="header__right-box">
+        </div>
+      </header>
+      <section class="section">
+        <input class="section__input" type="text" placeholder="키워드를 입력해주세요"/>
+        <div class="section__search-img" alt="search-icon"></div>
+        <div class="section__dropdown">
+          <div class="dropdown__card">
+            <div class="dropdown__card--icon"></div>
+            <div class="dropdown__card--text">차 종류</div>
+            <div class="dropdown__card--type">태그</div>
+          </div>
+        </div>
+      </section>  
+      <main class="main">
+        <div class="main__title">My Feed</div>
+        <div class="main__hashtage">
+          <div class="hashtage"># car</div>
+          <div class="hashtage"># car</div>
+          <div class="hashtage"># car</div>
+          <div class="hashtage"># car</div>
+          <div class="hashtage"># car</div>
+          <div class="hashtage"># car</div>
+        </div>
+        <div class="main__gallery">
+          <div class="main__gallery--image"></div>
+          <div class="main__gallery--image"></div>
+          <div class="main__gallery--image"></div>
+          <div class="main__gallery--image"></div>
+          <div class="main__gallery--image"></div>
+          <div class="main__gallery--image"></div>
+          <div class="main__gallery--image"></div>
+          <div class="main__gallery--image"></div>
+        </div>
+        <div class="main__button">+</div>
+      </main>
+    </div>
+    `;
   }
 }
