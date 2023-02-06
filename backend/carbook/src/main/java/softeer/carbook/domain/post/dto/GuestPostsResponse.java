f618b64a0992082e46555fb276e33506de1512a0
jaehunLee-dev <jaehunlee.dev@gmail.com>
2023-02-06 15:18:37 +0900

    feat: dto/GeustPostsResponse 생성 비로그인에서의 포스트 Response 클래스

--- backend/carbook/src/main/java/softeer/carbook/domain/post/dto/GuestPostsResponse.java
+++ backend/carbook/src/main/java/softeer/carbook/domain/post/dto/GuestPostsResponse.java
@@ -1,6 +1,16 @@
 package softeer.carbook.domain.post.dto;
 
-public class PostsResponse {
+import softeer.carbook.domain.post.model.Image;
+
+import java.util.List;
+import java.util.Map;
+
+public class GuestPostsResponse {
     private boolean isLogin;
-    private String 
+    private List<Image> images;
+
+    public GuestPostsResponse(boolean isLogin, List<Image> images){
+        this.isLogin = isLogin;
+        this.images = images;
+    }
 }
