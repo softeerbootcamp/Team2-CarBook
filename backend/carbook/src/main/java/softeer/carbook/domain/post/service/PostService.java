ba4b6eaf564ada25aa370e9a597439db868f5133
jaehunLee-dev <jaehunlee.dev@gmail.com>
2023-02-06 15:55:02 +0900

    feat: PostService/GuestPostsResponse 구현 비로그인 상태에서 최근 게시물 10개를 반환하는 메소드

--- backend/carbook/src/main/java/softeer/carbook/domain/post/service/PostService.java
+++ backend/carbook/src/main/java/softeer/carbook/domain/post/service/PostService.java
@@ -3,21 +3,35 @@ package softeer.carbook.domain.post.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import softeer.carbook.domain.post.dto.GuestPostsResponse;
+import softeer.carbook.domain.post.model.Image;
+import softeer.carbook.domain.post.model.Post;
+import softeer.carbook.domain.post.repository.ImageRepository;
 import softeer.carbook.domain.post.repository.PostRepository;
 import softeer.carbook.domain.user.dto.Message;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class PostService {
     PostRepository postRepository;
+    ImageRepository imageRepository;
     private final int POST_COUNT = 10;
 
     @Autowired
     public PostService(PostRepository postRepository) { this.postRepository = postRepository; }
 
-    /*
-    public ResponseEntity<Message> getRecentPosts(int index){
-
+    public GuestPostsResponse getRecentPosts(int index){
+        List<Post> posts = postRepository.getPosts(POST_COUNT, index);
+        List<Image> images = new ArrayList<>();
+        for (Post post: posts){
+            Image image = imageRepository.getImageByPostId(post.getId());
+            images.add(image);
+        }
+        return new GuestPostsResponse(false, images);
     }
 
-     */
 }
