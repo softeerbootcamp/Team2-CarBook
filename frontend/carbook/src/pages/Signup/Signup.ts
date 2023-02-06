159e7e7e879884a878ff94871af23c50107e7593
Dongja <jdh981118@naver.com>
2023-02-06 12:17:24 +0900

    💄design: add sign up page markup

--- frontend/carbook/src/pages/Signup/Signup.ts
+++ frontend/carbook/src/pages/Signup/Signup.ts
@@ -1,6 +1,31 @@
 import { Component } from '@/core';
+import './Signup.scss';
+import car from '@/assets/car.svg';
+
 export default class SignupPage extends Component {
   template(): string {
-    return `회원가입페이지 입니다`;
+    return `
+    <div class = 'signup-container'>
+    <header>
+    <img src = ${car} class ='header-image'>
+  <h1 class = 'header-logo'>CarBook</h1>
+  <h3 class = 'header-message'>카북에 오신걸 환영합니다</h3>
+  </header>
+  <div class='signup-contents'>
+    <form class ='input-form'>
+      <div class = 'signup-id'> ID</div>
+      <input type = 'text' placeholder='id를 입력해주세요' class ='input-box' name ='signup-id'/>
+      <div class ='signup-password'> Password</div>
+      <input type = 'password' class ='input-box' name = 'signup-password' placeholder='비밀번호를 입력해주세요'/>
+      <div class ='signup-nickname'> Nickname</div>
+      <input type = 'nickname' class ='input-box' name = 'signup-nickname' placeholder='닉네임을 입력해주세요'/>
+      <button type = 'submit' class ='input-form-button'>회원가입</button>
+    </form>
+  </div>
+  <footer>
+    <h3 class ='footer-message'>계정이 이미 있으신가요?</h3>
+    <h3 class ='footer-login'>로그인</h3>
+  </footer>
+  </div>`;
   }
 }
