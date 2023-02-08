9737bad42612913a9323c78cd59f1b926fa088e4
Dongja <jdh981118@naver.com>
2023-02-08 13:59:41 +0900

    feat: password, nickname 길이 16자 제한

--- frontend/carbook/src/pages/Signup/Signup.ts
+++ frontend/carbook/src/pages/Signup/Signup.ts
@@ -1,8 +1,15 @@
-import { Component } from '@/core';
-import './Signup.scss';
-import car from '@/assets/images/car.svg';
+import { Component } from "@/core";
+import "./Signup.scss";
+import car from "@/assets/images/car.svg";
 
 export default class SignupPage extends Component {
+  setup(): void {
+    this.setState({
+      // *더미 데이터
+      id: "dongja",
+      password: "qwer1234",
+    });
+  }
   template(): string {
     return `
     <div class = 'signup-container'>
@@ -16,9 +23,9 @@ export default class SignupPage extends Component {
       <div class = 'signup-id'> ID</div>
       <input type = 'text' placeholder='id를 입력해주세요' class ='input-box' name ='signup-id'/>
       <div class ='signup-password'> Password</div>
-      <input type = 'password' class ='input-box' name = 'signup-password' placeholder='비밀번호를 입력해주세요'/>
+      <input type = 'password' class ='input-box' name = 'signup-password' placeholder='비밀번호를 입력해주세요' maxlength='16'/>
       <div class ='signup-nickname'> Nickname</div>
-      <input type = 'nickname' class ='input-box' name = 'signup-nickname' placeholder='닉네임을 입력해주세요'/>
+      <input type = 'nickname' class ='input-box' name = 'signup-nickname' placeholder='닉네임을 입력해주세요' maxlength='16'/>
       <button type = 'submit' class ='input-form-button'>회원가입</button>
     </form>
   </div>
@@ -28,4 +35,6 @@ export default class SignupPage extends Component {
   </footer>
   </div>`;
   }
+
+  setEvent(): void {}
 }
