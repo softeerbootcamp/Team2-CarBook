2a69e20c1cb555acb29d13e63272b9d6e2297b7f
jaehunLee-dev <jaehunlee.dev@gmail.com>
2023-02-08 10:40:12 +0900

    refactor: GuestPostsResponse Builder 패턴 적용

--- backend/carbook/src/main/java/softeer/carbook/domain/post/dto/GuestPostsResponse.java
+++ backend/carbook/src/main/java/softeer/carbook/domain/post/dto/GuestPostsResponse.java
@@ -1,18 +1,31 @@
 package softeer.carbook.domain.post.dto;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import softeer.carbook.domain.post.model.Image;
 
 import java.util.List;
 
 public class GuestPostsResponse {
-    private boolean isLogin;
+    private final boolean isLogin = false;
     private List<Image> images;
 
-    public GuestPostsResponse(boolean isLogin, List<Image> images){
-        this.isLogin = isLogin;
-        this.images = images;
+    public GuestPostsResponse(GuestPostsResponseBuilder guestPostsResponseBuilder){
+        this.images = guestPostsResponseBuilder.images;
+    }
+
+    public static class GuestPostsResponseBuilder {
+        private List<Image> images;
+
+        public GuestPostsResponseBuilder() {
+        }
+
+        public GuestPostsResponse.GuestPostsResponseBuilder images(List<Image> images) {
+            this.images = images;
+            return this;
+        }
+
+        public GuestPostsResponse build() {
+            return new GuestPostsResponse(this);
+        }
     }
 
 }
