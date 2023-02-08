cda8197c990ccfaf00edc75242d923e6842103a9
jaehunLee-dev <jaehunlee.dev@gmail.com>
2023-02-08 11:59:35 +0900

    chore: pr과 merge

--- backend/carbook/src/test/java/softeer/carbook/domain/post/service/PostServiceTest.java
+++ backend/carbook/src/test/java/softeer/carbook/domain/post/service/PostServiceTest.java
@@ -2,6 +2,22 @@ package softeer.carbook.domain.post.service;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import softeer.carbook.domain.follow.repository.FollowRepository;
+import softeer.carbook.domain.post.dto.GuestPostsResponse;
+import softeer.carbook.domain.post.model.Image;
+import softeer.carbook.domain.user.controller.UserController;
+import softeer.carbook.domain.user.repository.UserRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +34,35 @@ class PostServiceTest {
 
     @Autowired
     PostService postService;
+
+    private final List<Image> images = new ArrayList<>(List.of(
+            new Image(8, "/eighth/image.jpg"),
+            new Image(7, "/seventh/image.jpg"),
+            new Image(6, "/sixth/image.jpg"),
+            new Image(5, "/fifth/image.jpg"),
+            new Image(4, "/fourth/image.jpg"),
+            new Image(3, "/third/image.jpg"),
+            new Image(2, "/second/image.jpg"),
+            new Image(1, "/first/image.jpg")
+    ));
+
+    @Test
+    @DisplayName("비로그인 상태 메인 페이지 테스트")
+    private void getRecentPostsTest() {
+        //given
+        int index = 0;
+        //when
+        GuestPostsResponse guestPostsResponse = postService.getRecentPosts(index);
+        //then
+        GuestPostsResponse expectedResult = new GuestPostsResponse.GuestPostsResponseBuilder()
+                .images(images)
+                .build();
+        assertThat(guestPostsResponse).usingRecursiveComparison().isEqualTo(expectedResult);
+    }
+
+    @Test
+    void getRecentFollowerPosts() {
+    }
 
     @Test
     @DisplayName("해시태그를 통한 게시물 검색 기능 테스트")
@@ -36,5 +81,13 @@ class PostServiceTest {
         assertThat(result.get(1).getImageUrl()).isEqualTo("/sixth/image.jpg");
         assertThat(result.get(2).getImageUrl()).isEqualTo("/third/image.jpg");
         assertThat(result.get(3).getImageUrl()).isEqualTo("/second/image.jpg");
+    }
+
+    @Test
+    void myProfile() {
+    }
+
+    @Test
+    void otherProfile() {
     }
 }
