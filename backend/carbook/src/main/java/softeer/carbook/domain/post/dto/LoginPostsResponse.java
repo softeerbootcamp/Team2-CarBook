7d0af8e2f98ee228f7d90685162e21a02552aad9
jaehunLee-dev <jaehunlee.dev@gmail.com>
2023-02-08 10:44:14 +0900

    refactor: LoginPostsResponse Builder 패턴 적용

--- backend/carbook/src/main/java/softeer/carbook/domain/post/dto/LoginPostsResponse.java
+++ backend/carbook/src/main/java/softeer/carbook/domain/post/dto/LoginPostsResponse.java
@@ -5,14 +5,36 @@ import softeer.carbook.domain.post.model.Image;
 import java.util.List;
 
 public class LoginPostsResponse {
-    private boolean isLogin;
+    private final boolean isLogin = true;
     private String nickname;
     private List<Image> images;
 
-    public LoginPostsResponse(boolean isLogin, String nickname, List<Image> images){
-        this.isLogin = isLogin;
-        this.nickname = nickname;
-        this.images = images;
+    public LoginPostsResponse(LoginPostsResponse.LoginPostsResponseBuilder loginPostsResponseBuilder){
+        this.nickname = loginPostsResponseBuilder.nickname;
+        this.images = loginPostsResponseBuilder.images;
     }
+
+    public static class LoginPostsResponseBuilder {
+        private String nickname;
+        private List<Image> images;
+
+        public LoginPostsResponseBuilder() {
+        }
+
+        public LoginPostsResponse.LoginPostsResponseBuilder nickname(String nickname) {
+            this.nickname = nickname;
+            return this;
+        }
+
+        public LoginPostsResponse.LoginPostsResponseBuilder images(List<Image> images) {
+            this.images = images;
+            return this;
+        }
+
+        public LoginPostsResponse build() {
+            return new LoginPostsResponse(this);
+        }
+    }
+
 
 }
