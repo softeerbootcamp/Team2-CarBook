cf36286cf91085750f1d6c861a2eebf04d4f1cd7
jhchoi57 <enough1125@gmail.com>
2023-02-08 17:54:34 +0900

    feat: ModifyPasswordForm password 필드에도 제약조건 추가

--- backend/carbook/src/main/java/softeer/carbook/domain/user/dto/ModifyPasswordForm.java
+++ backend/carbook/src/main/java/softeer/carbook/domain/user/dto/ModifyPasswordForm.java
@@ -4,6 +4,8 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 public class ModifyPasswordForm {
+    @Size(max = 16, message = "비밀번호는 16자 이하여야 합니다.")
+    @NotBlank(message = "비밀번호를 입력해 주세요.")
     private final String password;
     @Size(max = 16, message = "비밀번호는 16자 이하여야 합니다.")
     @NotBlank(message = "비밀번호를 입력해 주세요.")
