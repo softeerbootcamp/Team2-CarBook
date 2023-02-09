645fe92484c10615ffd23848abe4d80e3dd9106f
Dongja <jdh981118@naver.com>
2023-02-09 11:53:38 +0900

    feat: login 성공시 메인페이지로 이동

--- frontend/carbook/src/pages/Login/Login.ts
+++ frontend/carbook/src/pages/Login/Login.ts
@@ -1,10 +1,11 @@
-import { Component } from '@/core';
-import './Login.scss';
-import car from '@/assets/images/car.svg';
+import { Component } from "@/core";
+import "./Login.scss";
+import car from "@/assets/images/car.svg";
+import { push } from "@/utils/router/navigate";
 
 export default class LoginPage extends Component {
   template(): string {
-    return `
+    return /*html*/ `
     <div class="login__container">
       <header>
         <img src = ${car} class ='header-image'>
@@ -14,9 +15,9 @@ export default class LoginPage extends Component {
       <div class='login-contents'>
         <form class ='input-form'>
           <div class = 'login-id'> ID</div>
-          <input type = 'text' placeholder='id를 입력해주세요' class ='input-box' name ='login-id'/>
+          <input type = 'text' placeholder='id를 입력해주세요' class ='input-box' name ='id'/>
           <div class ='login-password'> Password</div>
-          <input type = 'password' class ='input-box' name = 'login-password' placeholder='비밀번호를 입력해주세요'/>
+          <input type = 'password' class ='input-box' name = 'password' placeholder='비밀번호를 입력해주세요'/>
           <button type = 'submit' class ='input-form-button'>로그인</button>
         </form>
       </div>
@@ -26,5 +27,25 @@ export default class LoginPage extends Component {
       </footer>
     </div>
     `;
+  }
+
+  setEvent(): void {
+    const form = document.body.querySelector(
+      ".login__container .input-form"
+    ) as HTMLFormElement;
+    console.log(form);
+    form?.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      const id = form.id.value.trim();
+      const password = form.password.value.trim();
+
+      const modal = document.body.querySelector(".alert-modal") as HTMLElement;
+
+      // if (isEmpty(id, password, modal)) return false;
+
+      push("/");
+      return false;
+    });
   }
 }
