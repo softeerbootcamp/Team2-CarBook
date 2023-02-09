28660c96461c439855b2b1f9cfa9221a689d9d31
Dongja <jdh981118@naver.com>
2023-02-09 11:24:45 +0900

    refactor: 에러메시지 상수 분리

---
+++ frontend/carbook/src/constants/errorMessage.ts
@@ -0,0 +1,15 @@
+export const [
+  EMPTYID,
+  EMPTYPW,
+  EMPTYNICKNAME,
+  DUPPLICATEDID,
+  DUPPLICATEDPW,
+  DUPPLICATEDNICKNAME,
+] = [
+  "ID를 입력해주세요",
+  "비밀번호를 입력해주세요",
+  "닉네임을 입력해주세요",
+  "중복된 ID입니다",
+  "중복된 비밀번호입니다",
+  "중복된 닉네임입니다",
+];
--- frontend/carbook/src/pages/Signup/Signup.ts
+++ frontend/carbook/src/pages/Signup/Signup.ts
@@ -2,22 +2,14 @@ import { Component } from "@/core";
 import "./Signup.scss";
 import car from "@/assets/images/car.svg";
 import { push } from "@/utils/router/navigate";
-
-const [
+import {
   EMPTYID,
   EMPTYPW,
   EMPTYNICKNAME,
   DUPPLICATEDID,
   DUPPLICATEDPW,
   DUPPLICATEDNICKNAME,
-] = [
-  "ID를 입력해주세요",
-  "비밀번호를 입력해주세요",
-  "닉네임을 입력해주세요",
-  "중복된 ID입니다",
-  "중복된 비밀번호입니다",
-  "중복된 닉네임입니다",
-];
+} from "@/constants/errorMessage";
 
 export default class SignupPage extends Component {
   setup(): void {
