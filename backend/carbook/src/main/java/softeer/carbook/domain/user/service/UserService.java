d618c430475a2b42ac6208ae79fb3a0ffe9861cd
jaehunLee-dev <jaehunlee.dev@gmail.com>
2023-02-09 17:13:18 +0900

    feat: UserService에 encryptPassword 메소드 추가 비밀번호 암호화 메소드

--- backend/carbook/src/main/java/softeer/carbook/domain/user/service/UserService.java
+++ backend/carbook/src/main/java/softeer/carbook/domain/user/service/UserService.java
@@ -1,5 +1,6 @@
 package softeer.carbook.domain.user.service;
 
+import org.mindrot.jbcrypt.BCrypt;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -125,5 +126,8 @@ public class UserService {
         return user.getPassword().equals(password);
     }
 
+    private String encryptPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
 
 }
