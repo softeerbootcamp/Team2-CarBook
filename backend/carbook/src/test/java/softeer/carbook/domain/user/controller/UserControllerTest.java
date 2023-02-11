c3c25978742f502a8ec8a7a4007a9da838715b62
jhchoi57 <enough1125@gmail.com>
2023-02-11 14:57:47 +0900

    test: 닉네임 변경 테스트코드 추가

--- backend/carbook/src/test/java/softeer/carbook/domain/user/controller/UserControllerTest.java
+++ backend/carbook/src/test/java/softeer/carbook/domain/user/controller/UserControllerTest.java
@@ -1,5 +1,7 @@
 package softeer.carbook.domain.user.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mysql.cj.x.protobuf.MysqlxDatatypes;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,15 +9,24 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import softeer.carbook.domain.user.dto.LoginForm;
 import softeer.carbook.domain.user.dto.Message;
+import softeer.carbook.domain.user.dto.ModifyNickNameForm;
 import softeer.carbook.domain.user.dto.SignupForm;
 import softeer.carbook.domain.user.service.UserService;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
@@ -44,7 +55,9 @@ class UserControllerTest {
                 .content("{\"email\":\"springtestemail@gmail.com\"," +
                         "\"nickname\":\"springtest\"," +
                         "\"password\":\"springtestpwd\"}"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("SignUp Success")));
+
     }
 
     @Test
@@ -63,16 +76,43 @@ class UserControllerTest {
         mockMvc.perform(post("/login")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"email\":\"springtestemail@gmail.com\"," +
-                                "\"password\":\"springtessdtpwd\"}"))
-                .andExpect(status().isOk());
+                                "\"password\":\"springtestpwd\"}"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Login Success")));
     }
 
     @Test
-    void logout() {
+    @DisplayName("로그아웃 테스트")
+    void logout() throws Exception{
+        // given
+        given(userService.logout(any(HttpServletRequest.class)))
+                .willReturn(new Message("Logout Success"));
+
+        // when & then
+        mockMvc.perform(post("/profile/logout")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Logout Success")));
     }
 
     @Test
-    void modifyNickname() {
+    @DisplayName("닉네임 변경 테스트")
+    void modifyNickname() throws Exception {
+        // given
+        String nickname = "springtest";
+        ModifyNickNameForm modifyNickNameForm = new ModifyNickNameForm("newspringtest");
+        given(userService.modifyNickname(
+                eq(nickname),
+                eq(modifyNickNameForm.getNewNickname()),
+                any(HttpServletRequest.class)))
+                .willReturn(new Message("Nickname modified successfully"));
+
+        // when & then
+        mockMvc.perform(patch("/profile/modify/{nickname}", "springtest")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"newNickname\":\"newspringtest\"}"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Nickname modified successfully")));
     }
 
     @Test
