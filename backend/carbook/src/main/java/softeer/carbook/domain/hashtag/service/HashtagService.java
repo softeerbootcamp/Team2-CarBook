c3bee23731f4199d41dea0d5591fd28d43ded33a
Soomin Lim <augustth511@gmail.com>
2023-02-11 23:47:24 +0900

    feat: 차량 타입 태그를 검색하는 기능 구현
    - searchType(): 인자로 주어진 키워드로 시작하는 차량 종류 태그를 검색하여 TypeSearchResponse를 반환

--- backend/carbook/src/main/java/softeer/carbook/domain/hashtag/service/HashtagService.java
+++ backend/carbook/src/main/java/softeer/carbook/domain/hashtag/service/HashtagService.java
@@ -3,7 +3,9 @@ package softeer.carbook.domain.hashtag.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import softeer.carbook.domain.hashtag.dto.HashtagSearchResponse;
+import softeer.carbook.domain.hashtag.dto.TypeSearchResponse;
 import softeer.carbook.domain.hashtag.model.Hashtag;
+import softeer.carbook.domain.hashtag.model.Type;
 import softeer.carbook.domain.hashtag.repository.HashtagRepository;
 
 import java.util.List;
@@ -23,6 +25,14 @@ public class HashtagService {
 
         return new HashtagSearchResponse.HashtagSearchResponseBuilder()
                 .hashtags(hashtags)
+                .build();
+    }
+
+    public TypeSearchResponse searchType(String keyword) {
+        List<Type> types = hashtagRepository.searchTypeByPrefix(keyword);
+
+        return new TypeSearchResponse.TypeSearchResponseBuilder()
+                .types(types)
                 .build();
     }
 }
