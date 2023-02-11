311a46c2c609ff44dc4c9fac1a8b6889272e6b75
Soomin Lim <augustth511@gmail.com>
2023-02-11 23:42:49 +0900

    feat: 접두어로 차량 종류를 검색하는 searchTypeByPrefix() 메서드 구현
    - 기존의 tagRowMapper() 메서드를 hashtagRowMapper()로 이름 변경
    - 쿼리 결과로 Type 객체를 매핑하는 typeRowMapper() 메서드 추가

--- backend/carbook/src/main/java/softeer/carbook/domain/hashtag/repository/HashtagRepository.java
+++ backend/carbook/src/main/java/softeer/carbook/domain/hashtag/repository/HashtagRepository.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import softeer.carbook.domain.hashtag.exception.HashtagNotExistException;
 import softeer.carbook.domain.hashtag.model.Hashtag;
+import softeer.carbook.domain.hashtag.model.Type;
 
 import javax.sql.DataSource;
 import java.util.List;
@@ -21,18 +22,29 @@ public class HashtagRepository {
     }
 
     public Hashtag findHashtagByName(String tag) {
-        List<Hashtag> hashtags = jdbcTemplate.query("SELECT h.id, h.tag FROM HASHTAG h WHERE tag = ?", tagRowMapper(), tag);
+        List<Hashtag> hashtags = jdbcTemplate.query("SELECT h.id, h.tag FROM HASHTAG h WHERE tag = ?", hashtagRowMapper(), tag);
         return hashtags.stream()
                 .findAny()
                 .orElseThrow(() -> new HashtagNotExistException());
     }
 
     public List<Hashtag> searchHashtagByPrefix(String keyword) {
-        return jdbcTemplate.query("SELECT h.id, h.tag FROM HASHTAG h WHERE tag LIKE '" + keyword + "%'", tagRowMapper());
+        return jdbcTemplate.query("SELECT h.id, h.tag FROM HASHTAG h WHERE tag LIKE '" + keyword + "%'", hashtagRowMapper());
     }
 
-    private RowMapper<Hashtag> tagRowMapper() {
+    public List<Type> searchTypeByPrefix(String keyword) {
+        return jdbcTemplate.query("SELECT t.id, t.tag FROM Type t WHERE tag LIKE '" + keyword + "%'", typeRowMapper());
+    }
+
+    private RowMapper<Hashtag> hashtagRowMapper() {
         return (rs, rowNum) -> new Hashtag(
+                rs.getInt("id"),
+                rs.getString("tag")
+        );
+    }
+
+    private RowMapper<Type> typeRowMapper() {
+        return (rs, rowNum) -> new Type(
                 rs.getInt("id"),
                 rs.getString("tag")
         );
