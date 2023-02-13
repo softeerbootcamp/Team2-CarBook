1e0e0c0052cb7d6129d313d00bc81f92e25388be
EUNA <67576476+beni1026@users.noreply.github.com>
2023-02-13 15:18:04 +0900

    feat: 차모델/종류 select 기능 (#179)

--- frontend/carbook/src/components/Post/Selection.ts
+++ frontend/carbook/src/components/Post/Selection.ts
@@ -1,18 +1,43 @@
 import { Component } from '@/core';
+import { qs } from '@/utils';
 
 export default class Selection extends Component {
+  setup(): void {
+    this.state = {
+      selected: '',
+    };
+  }
   template(): string {
     const { label, options } = this.props;
 
     return `
       <label>${label}</label>
-      <select name="type">
+      <select name="type" class="select">
+          <option>선택하세요</option>
         ${options.map(
-          (option: string) => `
-          <option>${option}</option>
-        `
+          (option: string) =>
+            `
+          <option value="${option}" 
+          ${this.setSelected(option)} >${option}</option>`
         )}
       </select>
     `;
+  }
+
+  setEvent(): void {
+    this.onHandleSelection();
+  }
+
+  onHandleSelection() {
+    const selection = qs(this.$target, 'select') as HTMLSelectElement;
+    selection.addEventListener('change', () => {
+      const selected = selection.options[selection.selectedIndex].text;
+      this.setState({ selected });
+    });
+  }
+
+  setSelected(option: string) {
+    const { selected } = this.state;
+    return selected === option ? 'selected' : '';
   }
 }
