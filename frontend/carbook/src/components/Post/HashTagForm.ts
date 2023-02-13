7087731cd6fad77b81cf432722e08603c02de633
EUNA <67576476+beni1026@users.noreply.github.com>
2023-02-13 18:48:12 +0900

    feat: 해시태그 검색 기능 (#186)
    * feat: 공통 함수 분리
    
    * feat: try-catch문 추가
    
    * feat: 해시태그 검색 기능

--- frontend/carbook/src/components/Post/HashTagForm.ts
+++ frontend/carbook/src/components/Post/HashTagForm.ts
@@ -1,21 +1,86 @@
 import { Component } from '@/core';
 import search from '@/assets/icons/search-blue.svg';
+import { onChangeInputHandler, onVisibleHandler, qs } from '@/utils';
+import { basicAPI } from '@/api';
+import SearchList from './SearchList';
+import { IHashTag } from '@/interfaces';
 
 export default class HashTagForm extends Component {
+  searchList: any;
+  setup(): void {
+    this.state = {
+      value: '',
+      hashtags: [],
+    };
+  }
   template(): string {
+    const { value, hashtags } = this.state;
+
     return `
       <div class="input__text">해시태그</div>
       <div class="input__box--hashtag">
         <div class="input__wrapper">
-          <input class="input" type="text" placeholder="해시태그를 검색해주세요"/>
+          <input class="input" value="${value}" type="text" placeholder="해시태그를 검색해주세요"/>
           <img src="${search}" alt="search-icon" class="icon"/>
         </div>
+        <div class="dropdown">
+        </div>
         <div class="hashtag__box">
-          <div class="hashtag"># car</div>
-          <div class="hashtag"># car</div>
-          <div class="hashtag"># car</div>
+          ${hashtags
+            .map(
+              ({ tag, type }: { tag: string; type: string }) =>
+                `<div class="hashtag ${type}"># ${tag}</div> `
+            )
+            .join('')}
         </div> 
       </div>
     `;
+  }
+
+  mounted(): void {
+    const dropdown = qs(this.$target, '.dropdown');
+    const searchList = new SearchList(dropdown, {
+      hashtags: [],
+      keyword: '',
+      addHashTag: (hashtag: IHashTag) => {
+        this.addHashTag(hashtag);
+      },
+    });
+    this.searchList = searchList;
+  }
+
+  setEvent(): void {
+    const form = qs(document, '.form');
+    const input = qs(this.$target, '.input') as HTMLInputElement;
+
+    form.addEventListener('click', ({ target }) => {
+      const dropdown = (<HTMLElement>target).closest('.dropdown');
+
+      if (dropdown) return;
+      onVisibleHandler(input, '.dropdown');
+    });
+
+    onChangeInputHandler(input, this.getSearchTags.bind(this));
+  }
+
+  addHashTag(hashtag: IHashTag) {
+    this.setState({ hashtags: [...this.state.hashtags, hashtag] });
+  }
+
+  async getSearchTags(keyword: string) {
+    const searchKeyword = keyword.trim();
+
+    if (searchKeyword.length !== 0) {
+      const searchedData = await basicAPI.get(
+        `/api/search/hashtag/?keyword=${keyword.trim()}`
+      );
+
+      this.searchList.setState({
+        hashtags: searchedData.data.hashtags,
+        keyword: searchKeyword,
+      });
+    } else {
+      this.searchList.setState({ hashtags: [], keyword: searchKeyword });
+    }
   }
 }
