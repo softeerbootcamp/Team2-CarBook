d79382cf12a979316e50f5b97e5facd55dff953f
Soomin-Lim <augustth511@gmail.com>
2023-02-13 17:38:54 +0900

    feat: 차량 종류 전체 조회 API 구현
    - getAllTypes(): 모든 차량 종류 정보를 조회하여 그에 대한 응답을 반환

--- backend/carbook/src/main/java/softeer/carbook/domain/hashtag/controller/HashtagController.java
+++ backend/carbook/src/main/java/softeer/carbook/domain/hashtag/controller/HashtagController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import softeer.carbook.domain.hashtag.dto.HashtagSearchResponse;
 import softeer.carbook.domain.hashtag.dto.TagSearchResopnse;
-import softeer.carbook.domain.hashtag.dto.TypeSearchResponse;
+import softeer.carbook.domain.hashtag.dto.TypesResponse;
 import softeer.carbook.domain.hashtag.service.HashtagService;
 
 @RestController
@@ -34,10 +34,10 @@ public class HashtagController {
         return new ResponseEntity<>(hashtagSearchResponse, HttpStatus.OK);
     }
 
-//    // 차량 종류 태그 검색
-//    @GetMapping("/search/type/")
-//    public ResponseEntity<TypeSearchResponse> searchType(@RequestParam String keyword) {
-//        TypeSearchResponse typeSearchResponse = hashtagService.searchType(keyword);
-//        return new ResponseEntity<>(typeSearchResponse, HttpStatus.OK);
-//    }
+    // 차량 종류 전체 조회
+    @GetMapping("/type")
+    public ResponseEntity<TypesResponse> getAllTypes() {
+        TypesResponse typesResponse = hashtagService.findAllTypes();
+        return new ResponseEntity<>(typesResponse, HttpStatus.OK);
+    }
 }
