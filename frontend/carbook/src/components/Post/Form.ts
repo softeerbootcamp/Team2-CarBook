4558ff84d25b79e33b5f0a7ebf3f8e5e26b12101
EUNA <67576476+beni1026@users.noreply.github.com>
2023-02-13 15:18:04 +0900

    feat: 차모델/종류 select 기능 (#179)

--- frontend/carbook/src/components/Post/Form.ts
+++ frontend/carbook/src/components/Post/Form.ts
@@ -1,6 +1,7 @@
 import { Component } from '@/core';
 import { ImageForm, Selection, HashTagForm, TextForm } from '@/components/Post';
 import { qs, qsa } from '@/utils';
+import { basicAPI } from '@/api';
 
 export default class Form extends Component {
   setup(): void {
@@ -39,22 +40,39 @@ export default class Form extends Component {
     `;
   }
 
-  mounted(): void {
+  async mounted() {
     const section = qs(this.$target, '.section');
     const inputBoxs = qsa(this.$target, '.input__box');
     const hastagInput = qs(this.$target, '.tag');
     const textInput = qs(this.$target, '.text');
 
     new ImageForm(section);
+
+    const { modelOption, typeOption } = await this.getCarOptions();
     new Selection(<HTMLElement>inputBoxs[0], {
       label: '차 종류',
-      options: [],
+      options: typeOption,
     });
     new Selection(<HTMLElement>inputBoxs[1], {
       label: '차 모델',
-      options: [],
+      options: modelOption,
     });
     new HashTagForm(hastagInput);
     new TextForm(textInput);
+  }
+
+  async getCarOptions() {
+    try {
+      const model = (await basicAPI.get('/api/model')).data();
+      const type = (await basicAPI.get('/api/type')).data();
+
+      const modelOption = model.types.map(({ tag }: { tag: string }) => tag);
+      const typeOption = type.types.map(({ tag }: { tag: string }) => tag);
+
+      return { modelOption, typeOption };
+    } catch (error) {
+      console.log(error);
+      return { modelOption: [], typeOption: [] };
+    }
   }
 }
