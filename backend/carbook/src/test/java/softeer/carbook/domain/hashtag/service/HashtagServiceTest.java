0dd3028e3ba5eabb8b4e5643058a8585bf241bc1
jhchoi57 <46276276+jhchoi57@users.noreply.github.com>
2023-02-13 15:25:13 +0900

    test: 기존 테스트 코드 수정 (#176)
    * test: UserService test 코드 수정
    
    * test: UserService test 코드 수정
    WebMvcTest >>> ExtendWith(MockitoExtension.class
    
    * test: PostService test 코드 수정
    
    * test: HashtagService test 코드 수정
    
    * test: 닉네임 변경 테스트코드 작성 중
    
    * test: profileUsernickname Get으로 받아오게 수정
    
    * hotfix: UserRepository getFollower, following 로직 수정
    
    * test: test코드 수정

--- backend/carbook/src/test/java/softeer/carbook/domain/hashtag/service/HashtagServiceTest.java
+++ backend/carbook/src/test/java/softeer/carbook/domain/hashtag/service/HashtagServiceTest.java
@@ -1,35 +1,54 @@
 package softeer.carbook.domain.hashtag.service;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import softeer.carbook.domain.hashtag.dto.HashtagSearchResponse;
 import softeer.carbook.domain.hashtag.model.Hashtag;
+import softeer.carbook.domain.hashtag.repository.HashtagRepository;
+import softeer.carbook.domain.post.service.PostService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
-@SpringBootTest
-@Transactional
+@ExtendWith(MockitoExtension.class)
 class HashtagServiceTest {
+    @InjectMocks
+    private HashtagService hashtagService;
 
-    @Autowired
-    HashtagService hashtagService;
+    @Mock
+    private HashtagRepository hashtagRepository;
 
     @ParameterizedTest
     @ValueSource(strings = {"맑", "맑음"})
     @DisplayName("해시태그 검색 기능 테스트")
     void searchHashTag(String keyword) {
+        // given
+        given(hashtagRepository.searchHashtagByPrefix(keyword)).willReturn(new ArrayList<>(List.of(
+                new Hashtag(1, "맑음")
+        )));
+
+        // when
         HashtagSearchResponse response = hashtagService.searchHashTag(keyword);
 
+        // then
         List<Hashtag> result = response.getHashtags();
         Hashtag hashtag = result.get(0);
         assertThat(result.size()).isEqualTo(1);
         assertThat(hashtag.getId()).isEqualTo(1);
         assertThat(hashtag.getTag()).isEqualTo("맑음");
+        verify(hashtagRepository).searchHashtagByPrefix(keyword);
     }
 }
