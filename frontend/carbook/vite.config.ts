264feefa8dd938db9c86a5734090477170ca5a53
jhchoi57 <46276276+jhchoi57@users.noreply.github.com>
2023-02-13 16:31:47 +0900

    v0.22 배포

--- frontend/carbook/vite.config.ts
+++ frontend/carbook/vite.config.ts
@@ -18,7 +18,7 @@ export default () =>
     server: {
       proxy: {
         '/api': {
-          target: 'http://3.39.1.92:8080',
+          target: 'http://localhost:8080',
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ''),
           secure: false,
