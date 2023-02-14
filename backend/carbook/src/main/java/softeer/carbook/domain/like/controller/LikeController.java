b8b309e935ba62ea156fe29d905f704432c2881a
jhchoi57 <enough1125@gmail.com>
2023-02-14 17:45:40 +0900

    feat: LikeController 로직 구현

--- backend/carbook/src/main/java/softeer/carbook/domain/like/controller/LikeController.java
+++ backend/carbook/src/main/java/softeer/carbook/domain/like/controller/LikeController.java
@@ -5,24 +5,44 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import softeer.carbook.domain.like.dto.ModifyLikeInfoForm;
 import softeer.carbook.domain.like.service.LikeService;
+import softeer.carbook.domain.user.model.User;
+import softeer.carbook.domain.user.service.UserService;
 import softeer.carbook.global.dto.Message;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class LikeController {
     private final LikeService likeService;
+    private final UserService userService;
 
     @Autowired
-    public LikeController(LikeService likeService){
+    public LikeController(
+            LikeService likeService,
+            UserService userService
+    ){
         this.likeService = likeService;
+        this.userService = userService;
     }
 
     // 좋아요
     // 좋아요 취소
     @PostMapping("/post/like")
     public ResponseEntity<Message> modifyLikeInfo(
-            @RequestBody
+            @RequestBody ModifyLikeInfoForm modifyLikeInfoForm,
+            HttpServletRequest httpServletRequest
     ){
+        // 일단 로그인 확인 후 로그인한 유저 받아오기
+        User loginUser = userService.findLoginedUser(httpServletRequest);
+
+        // 로그인한 유저 정보와 좋아요/좋아요취소를 할 게시글의 id를 파라미터로 전달
+        Message resultMsg = likeService.modifyLikeInfo(
+                loginUser,
+                httpServletRequest
+        );
+        return Message.make200Response(resultMsg.getMessage());
 
     }
 
--- backend/carbook/src/main/java/softeer/carbook/domain/like/service/LikeService.java
+++ backend/carbook/src/main/java/softeer/carbook/domain/like/service/LikeService.java
@@ -1,7 +1,14 @@
 package softeer.carbook.domain.like.service;
 
 import org.springframework.stereotype.Service;
+import softeer.carbook.domain.user.model.User;
+import softeer.carbook.global.dto.Message;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Service
 public class LikeService {
+    public Message modifyLikeInfo(User loginUser, HttpServletRequest httpServletRequest) {
+        return null;
+    }
 }
