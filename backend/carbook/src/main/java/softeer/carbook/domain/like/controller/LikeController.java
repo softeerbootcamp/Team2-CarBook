3ad70dbdbf7386cb9cdaede956d40f307cdc6304
jhchoi57 <enough1125@gmail.com>
2023-02-14 17:43:02 +0900

    feat: 좋아요 정보를 받을 dto 클래스 생성

--- backend/carbook/src/main/java/softeer/carbook/domain/like/controller/LikeController.java
+++ backend/carbook/src/main/java/softeer/carbook/domain/like/controller/LikeController.java
@@ -1,7 +1,29 @@
 package softeer.carbook.domain.like.controller;
 
-public class LikeController {
-    // 좋아요
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import softeer.carbook.domain.like.service.LikeService;
+import softeer.carbook.global.dto.Message;
 
+@RestController
+public class LikeController {
+    private final LikeService likeService;
+
+    @Autowired
+    public LikeController(LikeService likeService){
+        this.likeService = likeService;
+    }
+
+    // 좋아요
     // 좋아요 취소
+    @PostMapping("/post/like")
+    public ResponseEntity<Message> modifyLikeInfo(
+            @RequestBody
+    ){
+
+    }
+
 }
--- backend/carbook/src/main/java/softeer/carbook/domain/like/service/LikeService.java
+++ backend/carbook/src/main/java/softeer/carbook/domain/like/service/LikeService.java
@@ -1,4 +1,7 @@
 package softeer.carbook.domain.like.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class LikeService {
 }
