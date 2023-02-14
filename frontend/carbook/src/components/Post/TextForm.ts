d04fb8d627c58d560abccff08bacc23dcd9e15ec
EUNA <67576476+beni1026@users.noreply.github.com>
2023-02-14 10:28:11 +0900

    feat: textarea 입력 기능

--- frontend/carbook/src/components/Post/TextForm.ts
+++ frontend/carbook/src/components/Post/TextForm.ts
@@ -1,10 +1,31 @@
 import { Component } from '@/core';
+import { qs } from '@/utils';
 
 export default class TextForm extends Component {
+  setup(): void {
+    this.state = {
+      content: '',
+    };
+  }
   template(): string {
     return `
       <div class="input__text">글 내용</div>
       <textarea class="input__textarea" placeholder="글 내용을 입력해주세요"></textarea>
     `;
+  }
+
+  setEvent(): void {
+    this.onChangeHandler();
+  }
+
+  onChangeHandler() {
+    const textarea = qs(
+      this.$target,
+      '.input__textarea'
+    ) as HTMLTextAreaElement;
+
+    textarea.addEventListener('keyup', () => {
+      this.state.content = textarea.value;
+    });
   }
 }
