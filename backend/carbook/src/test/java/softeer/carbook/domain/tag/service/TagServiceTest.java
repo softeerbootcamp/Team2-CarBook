1ac61522ea3074e89c6c941e98ee0229e483dba7
Soomin Lim <63943319+Soomin-Lim@users.noreply.github.com>
2023-02-14 11:37:31 +0900

    test: TagService의 메서드를 테스트하느 코드 작성 (#190)
    - searchAllTags(): 모든 태그 검색 기능 테스트
    - findAllTypes(): 모든 차량 종류 조회 기능 테스트
    - findAllModels(): 모든 차량 모델 조회 기능 테스트

--- backend/carbook/src/test/java/softeer/carbook/domain/tag/service/TagServiceTest.java
+++ backend/carbook/src/test/java/softeer/carbook/domain/tag/service/TagServiceTest.java
@@ -1,14 +1,17 @@
 package softeer.carbook.domain.tag.service;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import softeer.carbook.domain.tag.dto.HashtagSearchResponse;
+import softeer.carbook.domain.tag.dto.*;
 import softeer.carbook.domain.tag.model.Hashtag;
+import softeer.carbook.domain.tag.model.Model;
+import softeer.carbook.domain.tag.model.Type;
 import softeer.carbook.domain.tag.repository.TagRepository;
 
 import java.util.ArrayList;
@@ -25,6 +28,37 @@ class TagServiceTest {
 
     @Mock
     private TagRepository tagRepository;
+
+    private final List<TagSearchResult> keywords = new ArrayList<>(List.of(
+            new TagSearchResult(1, "type", "승용"),
+            new TagSearchResult(1, "model", "아이오닉"),
+            new TagSearchResult(1, "hashtag", "맑음")
+    ));
+
+    @Test
+    @DisplayName("모든 태그 검색 기능 테스트")
+    void searchAllTags() {
+        // given
+        String keyword = "keyword";
+        given(tagRepository.searchTypeByPrefix(keyword)).willReturn(new ArrayList<>(List.of(
+                new Type(1, "승용")
+        )));
+        given(tagRepository.searchModelByPrefix(keyword)).willReturn(new ArrayList<>(List.of(
+                new Model(1, 1, "아이오닉")
+        )));
+        given(tagRepository.searchHashtagByPrefix(keyword)).willReturn(new ArrayList<>(List.of(
+                new Hashtag(1, "맑음")
+        )));
+
+        // when
+        TagSearchResopnse response = tagService.searchAllTags(keyword);
+
+        // then
+        assertThat(response.getKeywords()).usingRecursiveComparison().isEqualTo(keywords);
+        verify(tagRepository).searchTypeByPrefix(keyword);
+        verify(tagRepository).searchModelByPrefix(keyword);
+        verify(tagRepository).searchHashtagByPrefix(keyword);
+    }
 
     @ParameterizedTest
     @ValueSource(strings = {"맑", "맑음"})
@@ -45,5 +79,39 @@ class TagServiceTest {
         assertThat(hashtag.getId()).isEqualTo(1);
         assertThat(hashtag.getTag()).isEqualTo("맑음");
         verify(tagRepository).searchHashtagByPrefix(keyword);
+    }
+
+    @Test
+    @DisplayName("모든 차량 종류 조회 테스트")
+    void findAllTypes() {
+        //given
+        List<Type> expectedResult = new ArrayList<>(List.of(
+                new Type(1, "승용"), new Type(2, "SUV")));
+        given(tagRepository.findAllTypes()).willReturn(expectedResult);
+
+        //when
+        TypesResponse response = tagService.findAllTypes();
+
+        //then
+        List<Type> result = response.getTypes();
+        assertThat(result).usingRecursiveComparison().isEqualTo(expectedResult);
+        verify(tagRepository).findAllTypes();
+    }
+
+    @Test
+    @DisplayName("모든 차량 모델 조회 테스트")
+    void findAllModels() {
+        //given
+        List<Model> expectedResult = new ArrayList<>(List.of(
+                new Model(1, 1, "쏘나타"), new Model(2, 2, "아이오닉")));
+        given(tagRepository.findAllModels()).willReturn(expectedResult);
+
+        //when
+        ModelsResponse response = tagService.findAllModels();
+
+        //then
+        List<Model> result = response.getModels();
+        assertThat(result).usingRecursiveComparison().isEqualTo(expectedResult);
+        verify(tagRepository).findAllModels();
     }
 }
