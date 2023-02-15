7cb90774b7ecf417b4344628a31656c7db13827a
EUNA <67576476+beni1026@users.noreply.github.com>
2023-02-15 12:20:42 +0900

    feat: 에러메시지 추가

--- frontend/carbook/src/constants/errorMessage.ts
+++ frontend/carbook/src/constants/errorMessage.ts
@@ -7,23 +7,30 @@ export const [
   DUPPLICATEDNICKNAME,
   SUCCESSSIGNUP,
 ] = [
-  "ID를 입력해주세요",
-  "비밀번호를 입력해주세요",
-  "닉네임을 입력해주세요",
-  "중복된 ID입니다",
-  "중복된 비밀번호입니다",
-  "중복된 닉네임입니다",
-  "회원가입에 성공하셨습니다",
+  'ID를 입력해주세요',
+  '비밀번호를 입력해주세요',
+  '닉네임을 입력해주세요',
+  '중복된 ID입니다',
+  '중복된 비밀번호입니다',
+  '중복된 닉네임입니다',
+  '회원가입에 성공하셨습니다',
 ];
 
 export const [NONEXISTENTID, NONEXISTENTPW, LOGINSUCCESS] = [
-  "존재하지 않는 이메일입니다",
-  "존재하지 않는 비밀번호입니다",
-  "로그인에 성공하였습니다",
+  '존재하지 않는 이메일입니다',
+  '존재하지 않는 비밀번호입니다',
+  '로그인에 성공하였습니다',
 ];
 
 export const [EMPTYMODIFYPW, EMPTYMODIFYCONFIRMPW, NotMatchedPassword] = [
-  "변경할 비밀번호를 입력해주세요",
-  "변경할 비밀번호를 한번 더 입력해주세요",
-  "변경할 비밀번호가 일치하지 않습니다",
+  '변경할 비밀번호를 입력해주세요',
+  '변경할 비밀번호를 한번 더 입력해주세요',
+  '변경할 비밀번호가 일치하지 않습니다',
+];
+
+export const [NOTIMAGEFILE, EMPTYCARTYPE, EMPTYCARMODEL, SESSIONTIMEOUT] = [
+  '이미지를 등록해주세요',
+  '차 종류를 선택해주세요',
+  '차 모델을 선택해주세요',
+  '다시 로그인 해주세요',
 ];
