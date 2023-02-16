7d919ba8c3e7def4d3a5b5e04bc6042c7b76d889
Dongja <jdh981118@naver.com>
2023-02-16 12:07:19 +0900

    feat: 내 게시글에서만 수정, 삭제 가능

--- frontend/carbook/src/pages/PostDetail/PostDetail.ts
+++ frontend/carbook/src/pages/PostDetail/PostDetail.ts
@@ -4,6 +4,7 @@ import backButton from "@/assets/icons/backButton.svg";
 import profile from "@/assets/icons/postdetail_profile.svg";
 import menu from "@/assets/icons/postdetail_menu.svg";
 import likeButton from "@/assets/icons/likeButton.svg";
+import notLikeButton from "@/assets/icons/notLikeButton.svg";
 
 export default class PostDetailPage extends Component {
   setup(): void {
@@ -15,6 +16,24 @@ export default class PostDetailPage extends Component {
       const menuItems = menu.querySelector(".info-menu-items") as HTMLElement;
       menuItems?.classList.toggle("FadeInAndOut");
     });
+
+    this.state = {
+      isMyPost: true,
+      nickname: "유저닉네임",
+      imageUrl: "이미지 url",
+      likeCount: 23, // 좋아요 개수
+      like: false,
+      createDate: "2023.02.01. 11:03", // 작성 일자
+      updateDate: "2023.02.02. 03:24", // 최근 수정 일자
+      type: "차 종류",
+      model: "차 모델",
+      keywords: [
+        { id: "1", category: "hashtag", tag: "이름" },
+        { id: "2", category: "type", tag: "이름" },
+        { id: "3", category: "model", tag: "이름" },
+      ],
+      content: "글 내용",
+    };
   }
 
   template(): string {
@@ -25,42 +44,94 @@ export default class PostDetailPage extends Component {
 
       <div class ='postdetail-info'>
         <div class ='info-header'>
-          <div class='info-profile'>
-            <img class ='user-img' src = "${profile}"/>
-            <h2 class = 'user-nickname'>유저 닉네임</h2>
-          </div>
-          <div class ='info-menu'><img src = "${menu}"/>
-            <ul class = 'info-menu-items'>
-              <li>메뉴</li>
-              <li>수정</li>
-              <li>삭제</li>
-            </ul>
-          </div>
         </div>
         <div class = 'info-contents'>
-          <div class ='info-tag-cards'>
-            <div class ='info-model-card'>차 모델명</div>
-            <div class ='info-type-card'>차 종류</div>
-          </div>
-          <div class ='info-hashtags'>
-            <div class ='info-hashtag'>#rolem</div>
-            <div class ='info-hashtag'>#rolem</div>
-            <div class ='info-hashtag'>#rolem</div>
-          </div>
-          <p class = 'info-description'>별하 다솜 여우비 로운 산들림 나비잠 감사합니다 책방 아리아 컴퓨터 가온해 늘품 도서 여우비 이플 도서관 나비잠 바나나 바람꽃 도서 바나나 예그리나 별하 소록소록 산들림 책방 바람꽃 소록소록 비나리 포도 감사합니다 함초롱하다 산들림 도담도담 바나나 가온누리 별하 산들림 그루잠 예그리나 도서 도서 바나나 함초롱하다 우리는 옅구름 도르레 산들림 옅구름 여우별.</p>
         </div>
       </div>
       
       <footer class ='postdetail-footer'>
-        <div class ='like-info'>
-          <div class ='like-button'><img src="${likeButton}"/></div>
-          <div class ='like-count'>23 likes</div>
-        </div>
-        <div class ='posted-time'>2023.02.01. 11:03</div>
       </footer>
 
       <div class = 'back-button'> <img class = 'backbutton' src = "${backButton}"/> </div>
     </div>
     `;
+  }
+  mounted(): void {
+    const infoHeader = this.$target.querySelector(
+      ".info-header"
+    ) as HTMLElement;
+    const infoContents = this.$target.querySelector(
+      ".info-contents"
+    ) as HTMLElement;
+    const footer = this.$target.querySelector(
+      ".postdetail-footer"
+    ) as HTMLElement;
+    new Footer(footer, {
+      like: this.state.like,
+      likeCount: this.state.likeCount,
+      createDate: this.state.createDate,
+      updateDate: this.state.updateDate,
+    });
+    new InfoContents(infoContents, {
+      type: this.state.type,
+      model: this.state.model,
+      content: this.state.content,
+    });
+    new InfoHeader(infoHeader, {
+      nickname: this.state.nickname,
+      isMyPost: this.state.isMyPost,
+    });
+  }
+}
+
+class InfoHeader extends Component {
+  template(): string {
+    const { isMyPost, nickname } = this.props;
+    return /*html*/ `
+    <div class='info-profile'>
+      <img class ='user-img' src = "${profile}"/>
+      <h2 class = 'user-nickname'>${nickname}</h2>
+    </div>
+    <div class ='info-menu ${isMyPost ? "" : "hidden"}'>
+      <ul class = 'info-menu-items'>
+        <li>메뉴</li>
+        <li>수정</li>
+        <li>삭제</li>
+      </ul>
+    </div>
+    `;
+  }
+}
+
+class InfoContents extends Component {
+  template(): string {
+    const { type, model, content } = this.props;
+    return /*html*/ `
+      <div class ='info-tag-cards'>
+      <div class ='info-type-card'>${type}</div>
+      <div class ='info-model-card'>${model}</div>
+    </div>
+    <div class ='info-hashtags'>
+      <div class ='info-hashtag'>#rolem</div>
+      <div class ='info-hashtag'>#rolem</div>
+      <div class ='info-hashtag'>#rolem</div>
+    </div>
+    <p class = 'info-description'>${content}</p>
+      `;
+  }
+}
+
+class Footer extends Component {
+  template(): string {
+    const { like, likeCount, createDate, updateDate } = this.props;
+    return /*html*/ `
+      <div class ='like-info'>
+        <div class ='like-button'><img src="${
+          like ? likeButton : notLikeButton
+        }"/></div>
+        <div class ='like-count'>${likeCount} likes</div>
+      </div>
+      <div class ='posted-time'>${createDate}</div>
+      `;
   }
 }
