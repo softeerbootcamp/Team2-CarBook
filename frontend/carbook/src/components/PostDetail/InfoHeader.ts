68f6c88c1fd3076217dda8240b698ada660a7056
Dongja <43432783+DongjaJ@users.noreply.github.com>
2023-02-16 18:22:42 +0900

    feat: 상세 페이지 연결 (#242)
    * feat: 글 작성자 누르면 프로필로 이동
    
    * feat: 뒤로가기 버튼 기능 추가
    
    * feat: 이미지 연
    
    * feat: 이미지 연
    
    * refactor: fetch함수 수정

--- frontend/carbook/src/components/PostDetail/InfoHeader.ts
+++ frontend/carbook/src/components/PostDetail/InfoHeader.ts
@@ -1,7 +1,34 @@
 import { Component } from "@/core";
 import profile from "@/assets/icons/postdetail_profile.svg";
+import { push } from "@/utils/router/navigate";
+import { getClosest } from "@/utils";
 
 export default class InfoHeader extends Component {
+  setup(): void {
+    const { nickname } = this.props;
+    this.$target.addEventListener("click", (e: Event) => {
+      const target = e.target as HTMLElement;
+
+      console.log(target);
+      if (target.classList.contains("modify-post")) {
+        const postid = location.pathname.split("/").slice(-1)[0];
+        push(`/post/${postid}/edit`);
+        return;
+      }
+
+      if (target.classList.contains("delete-post")) {
+        // const postid = location.pathname.split("/").slice(-1)[0];
+        // push(`/post/${postid}/edit`);
+        // return;
+        console.log("삭제요청");
+        return;
+      }
+
+      if (getClosest(target, ".info-profile")) {
+        push(`/profile/${nickname}`);
+      }
+    });
+  }
   template(): string {
     const { isMyPost, nickname } = this.props;
     return /*html*/ `
@@ -12,8 +39,8 @@ export default class InfoHeader extends Component {
     <div class ='info-menu ${isMyPost ? "" : "hidden"}'>
       <ul class = 'info-menu-items'>
         <li>메뉴</li>
-        <li>수정</li>
-        <li>삭제</li>
+        <li class = 'modify-post'>수정</li>
+        <li class = 'delete-post'>삭제</li>
       </ul>
     </div>
     `;
