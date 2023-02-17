07545d813a386fc8313db12e57c9ed530298e99d
EUNA <67576476+beni1026@users.noreply.github.com>
2023-02-17 18:46:11 +0900

    fix: 페이지간 이벤트 쌓이는 문제 수정

--- frontend/carbook/src/utils/router/index.ts
+++ frontend/carbook/src/utils/router/index.ts
@@ -18,14 +18,21 @@ export default class Router {
 
   private route() {
     const TargetPage = this.findMatchedRoute()?.element || NotFoundPage;
-    new TargetPage(this.$target);
+
+    document.body.innerHTML = '';
+    const app = document.createElement('div');
+    app.id = 'app';
+    document.body.appendChild(app);
+
+    new TargetPage(app);
   }
 
   private initRouter() {
     window.addEventListener(ROUTE_CHNAGE_EVENT, (e: Event) => {
       const { nextUrl, isReplace } = (<CustomEvent>e).detail;
 
-      if (isReplace || nextUrl === location.pathname) history.replaceState(null, '', nextUrl);
+      if (isReplace || nextUrl === location.pathname)
+        history.replaceState(null, '', nextUrl);
       else history.pushState(null, '', nextUrl);
 
       this.route();
