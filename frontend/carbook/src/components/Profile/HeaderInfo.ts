6686dd6d712161e4c969418c7d0df7d39d41a24b
Dongja <jdh981118@naver.com>
2023-02-21 17:15:57 +0900

    feat: home button, 프로필 페이지 새로고침 기능 추가

--- frontend/carbook/src/components/Profile/HeaderInfo.ts
+++ frontend/carbook/src/components/Profile/HeaderInfo.ts
@@ -1,27 +1,50 @@
-import { Component } from "@/core";
-import FollowButton from "./FollowButton";
-import Menu from "./Menu";
+import { Component } from '@/core';
+import FollowButton from './FollowButton';
+import Menu from './Menu';
+import { getClosest } from '@/utils';
+import { push } from '@/utils/router/navigate';
+
 export default class HeaderInfo extends Component {
   template(): string {
     const { nickname, email } = this.props;
     return /*html*/ `
       <div class = 'header-info-header'>
-        <div class ='user-nickname'>${nickname ?? ""}</div>
+        <div class ='user-nickname'>
+        <div class = 'home'></div>
+          ${nickname ?? ''}
+        </div>
         <div class ='modify-status'></div>
       </div>
-      <div class ='user-email'>${email ?? ""}</div>
+      <div class ='user-email'>${email ?? ''}</div>
       `;
   }
 
   mounted(): void {
     const { isMyProfile, isFollow, nickname } = this.props;
     const modify_status = this.$target.querySelector(
-      ".modify-status"
+      '.modify-status'
     ) as HTMLElement;
 
     if (isMyProfile === undefined) return;
     isMyProfile
       ? new Menu(modify_status, { nickname })
       : new FollowButton(modify_status, { isFollow });
+  }
+
+  setEvent(): void {
+    const { nickname } = this.props;
+    const headerInfo = this.$target.querySelector(
+      '.header-info-header'
+    ) as HTMLElement;
+    headerInfo?.addEventListener('click', (e: Event) => {
+      const target = e.target as HTMLElement;
+      const home = getClosest(target, '.home');
+      const nicknameDiv = getClosest(target, '.user-nickname');
+      if (home) {
+        push('/');
+        return;
+      }
+      if (nicknameDiv) push(`/profile/${nickname}`);
+    });
   }
 }
